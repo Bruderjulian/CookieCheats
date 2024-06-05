c737442b8528eb5b95c31f00de7930549dc204fb
BruderJulian <julian@familie-zech.de>
2024-06-05 16:56:08 +0200

    Initial

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-
-f
+if (cheats !== undefined) cheats.stop();
+var cheats = {
+  speedSlow: 1000,
+  speedFast: 1,
+  start: function () {
+    var shimmers = document.getElementsByClassName("shimmer");
+    var bigCookie = document.getElementById("bigCookie");
+    cheats.intervalCookie = setInterval(() => {
+      bigCookie.click();
+    }, speedFast);
+    cheats.intervalShimmer = setInterval(() => {
+      for (let i = 0, l = shimmers.length; i < l; i++) {
+        shimmers[i].click();
+      }
+    }, 1000);
+  },
+  stop: function () {
+    if (cheats.intervalCookie) clearInterval(cheats.intervalCookie);
+    if (cheats.intervalShimmer) clearInterval(cheats.intervalShimmer);
+  },
+};
